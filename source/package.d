687e5b019c5578da11ab4b4934a871b4b43652e9
Chibisi Chima-Okereke <chibisi@active-analytics.com>
2017-02-23 15:44:38 +0000

    tidy up

--- source/package.d
+++ source/package.d
@@ -98,12 +98,24 @@ unittest
 	assert(gsl_stats_absdev(some_numbers.ptr, 1, some_numbers.length) == 2.5);
 }
 
-
-int main(){
-    import std.range : iota, array;
+/*
+void main(){
+	import std.range : iota, array;
     import std.stdio : writeln;
 
-	writeln("testing ...");
-	
-    return 0;
+    double[] x;
+    import gsl.randist : gsl_ran_gaussian_pdf;
+    import gsl.cdf : gsl_cdf_gaussian_P;
+    writeln("PDF, x = 0: ", gsl_ran_gaussian_pdf(0, 1));
+    writeln("CDF, x = 0: ", gsl_cdf_gaussian_P (0, 1));
+    writeln("CDF, x = -1.96: ", gsl_cdf_gaussian_P (-1.96, 1));
+    writeln("CDF, x = 1.96: ", gsl_cdf_gaussian_P (1.96, 1));
+
+    import gsl.statistics : gsl_stats_mean, gsl_stats_variance, gsl_stats_sd, gsl_stats_absdev;
+    double[10] some_numbers = iota(1., 11., 1.).array();
+    writeln("\n", gsl_stats_mean(some_numbers.ptr, 1, some_numbers.length));
+    writeln(gsl_stats_sd(some_numbers.ptr, 1, some_numbers.length));
+    writeln(gsl_stats_variance(some_numbers.ptr, 1, some_numbers.length));
+    writeln(gsl_stats_absdev(some_numbers.ptr, 1, some_numbers.length));
 }
+*/
